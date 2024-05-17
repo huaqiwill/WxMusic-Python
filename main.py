256cb7c317cc7206d49857eb46e339274dc56d35
huaqiwill <3173484026@qq.com>
2024-05-18 01:29:07 +0800

    集中更新

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-# @Project: wxMusic
-# @File: main.py 这是程序的启动文件
-# @Author: CunFu Peng
-# @Created: 2021-09-18
-# @Updated: 2021-11-24
-# @Version: 2.1
-# 五音助手
+"""
+Project: wxMusic
+File: main.py 这是程序的启动文件
+Author: CunFu Peng
+Created: 2021-09-18
+Updated: 2021-11-24
+Version: 2.1
+五音助手
+
+一款集优秀的音乐播放器，并且内置酷狗音乐、网易云音乐、等源
+"""
 
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
@@ -18,4 +22,3 @@ if __name__ == "__main__":
     win.show()
     print("启动成功！")
     sys.exit(app.exec_())
-    
